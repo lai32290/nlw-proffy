3b085917e1106c2d9f72c113cc1e102ae64bc337
Lai Xuancheng <lai32690@gmail.com>
2020-08-08 07:00:08 -0300

    create server

---
+++ server/README.md
@@ -0,0 +1,13 @@
+#Funcionalidade
+
+
+## Conexoes
+
+- Rota para listar total de conexoes.
+- Rota para criar uma conexao.
+
+## Aulas
+- Rota para criar uma aula.
+- Rota para listar aulas.
+    - Filtrar por materia, dia da semana e horario.
+
---
+++ server/src/routes.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import ClassesController from './controllers/classes-controller';
+import ConnectionsController from './controllers/connections-controller';
+
+const routes = express.Router();
+const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
+
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
+
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
+
+
+export default routes;
