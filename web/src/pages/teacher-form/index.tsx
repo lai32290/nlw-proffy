a3410122a60dd51f48d7a2003cd1224b71e0418d
Lai Xuancheng <lai32690@gmail.com>
2020-08-08 10:07:40 -0300

    creating teacher form page

---
+++ web/src/pages/teacher-form/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PageHeader from '../../components/page-header';
+
+import './style.css';
+
+function TeacherForm() {
+    return (
+        <div id="page-teacher-form" className="container">
+            <PageHeader
+                title="Que incrivel que você quer dar aulas."
+                description="O primeiro passo é preencher esse formulário de inscrição" />
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+
+                    <div className="input-block">
+                        <label htmlFor="name">Nome completo</label>
+                        <input type="text" id="name" />
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="avatar">Avatar</label>
+                        <input type="text" id="avatar" />
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="whatsapp">WhatsApp</label>
+                        <input type="text" id="whatsapp" />
+                    </div>
+                </fieldset>
+            </main>
+        </div>
+    );
+}
+
+export default TeacherForm;
